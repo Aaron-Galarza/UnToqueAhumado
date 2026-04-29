9ab16fb93b1faf67fca3594cc9bfec119cb65dc5
Aaron-Galarza <aarongalarza2005@gmail.com>
2026-04-29 15:57:08 -0300

    feat: origins permitidos en CORS

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,7 +6,26 @@ import { errorHandler } from './middlewares/error.middleware';
 
 const app: Application = express();
 
-app.use(cors());
+const allowedOrigins = [
+    'https://un-toque-ahumado.vercel.app',
+    'http://localhost:3000'
+]
+
+app.use(cors({
+    origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json())
 app.use(requestLogger)
 
