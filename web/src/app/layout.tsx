7079ac25906bdcbb18e7c221fbb19a716cdeb083
facusalass <facusalass15@gmail.com>
2026-05-05 21:13:30 -0300

    Prueba de fix para fuentes

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        {/* 👇 PLAN B: Carga rápida sin bloquear el CSS 👇 */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
+      </head>
       <body className="antialiased bg-background text-foreground flex flex-col min-h-screen">
         
         <Header />
